b43be61054afe327e01db6c194b3663e38773f32
Karotte128 <karotte128@outlook.de>
2026-09-02 23:26:50 +0200

    load basePermissions at permission check instead of injecting in the middleware

--- simpleauth/authMiddleware.go
+++ simpleauth/authMiddleware.go
@@ -2,10 +2,8 @@ package simpleauth
 
 import (
 	"net/http"
-	"slices"
 
 	"github.com/karotte128/karotteapi/v2/api"
-	"github.com/karotte128/karottelib/config"
 )
 
 var authMiddleware = api.Middleware{
@@ -17,12 +15,6 @@ var authMiddleware = api.Middleware{
 
 func authHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg, ok := api.GetMiddlewareConfig("auth")
-		if !ok {
-			http.Error(w, "Internal Server Error: No middleware config!", http.StatusInternalServerError)
-			return
-		}
-
 		if authProvider.ReadAuthInfo == nil {
 			http.Error(w, "Internal Server Error: Auth provider not set!", http.StatusInternalServerError)
 			return
@@ -39,16 +31,6 @@ func authHandler(next http.Handler) http.Handler {
 			http.Error(w, "Internal Server Error: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
-
-		basePermissions, ok := config.GetNestedValue[[]string](cfg, "basePermissions")
-		if !ok {
-			http.Error(w, "Internal Server Error: Config value basePermissions not set!", http.StatusInternalServerError)
-			return
-		}
-
-		combined := slices.Concat(authInfo.Permissions, basePermissions)
-		slices.Sort(combined)
-		authInfo.Permissions = slices.Compact(combined)
 
 		setAuthInfo(r, &authInfo)
 
--- simpleauth/permission.go
+++ simpleauth/permission.go
@@ -1,8 +1,13 @@
 package simpleauth
 
 import (
+	"errors"
+	"slices"
 	"strings"
 	"time"
+
+	"github.com/karotte128/karotteapi/v2/api"
+	"github.com/karotte128/karottelib/config"
 )
 
 func permissionMatch(pattern string, permission string) bool {
@@ -23,8 +28,7 @@ func permissionMatch(pattern string, permission string) bool {
 
 	// Exactly one wildcard
 	prefix, suffix, _ := strings.Cut(pattern, "*")
-	return strings.HasPrefix(permission, prefix) &&
-		strings.HasSuffix(permission, suffix)
+	return strings.HasPrefix(permission, prefix) && strings.HasSuffix(permission, suffix)
 }
 
 func checkPermission(info AuthInfo, requiredPerm string) bool {
@@ -38,10 +42,24 @@ func checkPermission(info AuthInfo, requiredPerm string) bool {
 }
 
 // This function checks if the AuthInfo of a request has the given permission.
-func HasPermission(info AuthInfo, perm string) bool {
+func HasPermission(info AuthInfo, perm string) (bool, error) {
 	if info.ValidUntil.Before(time.Now()) {
-		return false
+		return false, nil
 	}
 
-	return checkPermission(info, perm)
+	cfg, ok := api.GetMiddlewareConfig("auth")
+	if !ok {
+		return false, errors.New("No middleware config!")
+	}
+
+	basePermissions, ok := config.GetNestedValue[[]string](cfg, "basePermissions")
+	if !ok {
+		return false, errors.New("Internal Server Error: Config value basePermissions not set!")
+	}
+
+	combined := slices.Concat(info.Permissions, basePermissions)
+	slices.Sort(combined)
+	info.Permissions = slices.Compact(combined)
+
+	return checkPermission(info, perm), nil
 }
